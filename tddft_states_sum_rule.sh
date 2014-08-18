ac6df4a899845569b828568fc45c4689ddd24da4
Jarvist Moore Frost <jarvist@fakemail.fake>
2014-08-18 14:26:53 +0100

    Make use of more HOMO states...

--- tddft_states_sum_rule.sh
+++ tddft_states_sum_rule.sh
@@ -4,7 +4,11 @@
 for FILE
 do
     echo -n "${FILE} HOMOs "
-    grep "occ. eigenvalues" ${FILE} | tail -n1 | awk '{for (i=5;i<=NF;i++) {printf("%f ",($i-$NF)*27.211)}}'
+    grep "occ. eigenvalues" ${FILE} | tail -n3 | #  Print out last three lines of occupied eigenvalues, as one line with spaces 
+    awk 'BEGIN{ORS=" "}{print $5,$6,$7,$8,$9}' | # dump all the text & just keep the numbers
+    awk '{for (i=5;i<=NF;i++) {printf("%f ",($i-$NF)*27.211)}}' #print them all out relative to the HOMO, in eV
+    # WHAT I LACK IN STYLE I MAKE UP FOR IN INEXACITUDE
+
     echo
 
     echo -n "${FILE} LUMOs "
