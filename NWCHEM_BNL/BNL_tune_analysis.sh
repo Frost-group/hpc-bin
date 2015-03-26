aadac78b78988ad7728f8d4a070a3d312d250428
Jarvist Moore Frost <jarvist@gmail.com>
2015-03-26 01:01:53 +0000

    Slightly updated horror show of a script to work with varying electron num...

--- NWCHEM_BNL/BNL_tune_analysis.sh
+++ NWCHEM_BNL/BNL_tune_analysis.sh
@@ -9,7 +9,7 @@ do
 
     CAM=` grep " cam " "${i}" | sed -n 1p | awk '{print $2}' `
 
-#    echo "${i} Electrons: $ELECTRONS Energy: $ENERGY"
+    echo "${i} Electrons: $ELECTRONS Energy: $ENERGY"
 
 #Molecular orbitals we get
 #Neutral: Just alpha
@@ -17,16 +17,16 @@ do
 #Anion: Alpha then Beta
 #Believe we just want the Alpha orbitals?
 
-    echo -n "${i} ${CAM}"
-#echo "NEUTRAL"
-    grep "Vector  ${ELECTRONS}" "${i}" | sed -n 1p | sed s/=/\ /g | sed s/D/E/g | awk '{printf(" %f ",$6*27.211)}'
+    echo -n "${i} ${CAM} "
+echo "NEUTRAL"
+    grep "Vector\s\+${ELECTRONS}" "${i}" | sed -n 1p | sed s/=/\ /g | sed s/D/E/g | awk '{printf(" %f ",$6*27.211)}'
 let LUMO=${ELECTRONS}+1
-    grep "Vector  ${LUMO}" "${i}" | sed -n 1p | sed s/=/\ /g | sed s/D/E/g | awk '{printf(" %f ",$6*27.211)}'
-#echo "CATION"
-#    grep "Vector  ${ELECTRONS}" "${i}" | sed -n 2p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
+    grep "Vector\s\+${LUMO}" "${i}" | sed -n 1p | sed s/=/\ /g | sed s/D/E/g | awk '{printf(" %f ",$6*27.211)}'
+echo "CATION"
+#    grep "Vector\s\+${ELECTRONS}" "${i}" | sed -n 2p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
 grep "Total DFT energy" "${i}" | sed -n 2p | awk '{printf (" %f ",27.211*-($5-('$ENERGY')))}'
-#echo "ANION"
-#    grep "Vector  ${ELECTRONS+1}" "${i}" | sed -n 4p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
+echo "ANION"
+#    grep "Vector\s\+${ELECTRONS+1}" "${i}" | sed -n 4p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
 grep "Total DFT energy" "${i}" | sed -n 3p | awk '{printf (" %f ",27.211*($5-('$ENERGY')))}'
     echo
     #    grep "Root  1" "${i}" 
--- NWCHEM_BNL/BNL_tune_analysis_deltas.sh
+++ NWCHEM_BNL/BNL_tune_analysis_deltas.sh
@@ -9,7 +9,7 @@ do
 
     CAM=` grep " cam " "${i}" | sed -n 1p | awk '{print $2}' `
 
-#    echo "${i} Electrons: $ELECTRONS Energy: $ENERGY"
+    echo "${i} Electrons: $ELECTRONS Energy: $ENERGY"
 
 #Molecular orbitals we get
 #Neutral: Just alpha
@@ -17,12 +17,12 @@ do
 #Anion: Alpha then Beta
 #Believe we just want the Alpha orbitals?
 
-    echo -n "${i} ${CAM}"
+    echo -n "${i} ${CAM} "
 #echo "NEUTRAL"
-    H=` grep "Vector  ${ELECTRONS}" "${i}" | sed -n 1p | sed s/=/\ /g | awk '{printf(" %f ",$6*2.7211)}' `
+    H=` grep "Vector\s\+${ELECTRONS}" "${i}" | sed -n 1p | sed s/=/\ /g | awk '{printf(" %f ",$6*2.7211)}' `
     let LUMO=${ELECTRONS}+1
 
-    L=` grep "Vector  ${LUMO}" "${i}" | sed -n 1p | sed s/=/\ /g | awk '{printf(" %f ",$6*0.27211)}' `
+    L=` grep "Vector\s\+${LUMO}" "${i}" | sed -n 1p | sed s/=/\ /g | awk '{printf(" %f ",$6*0.27211)}' `
     #echo "CATION"
 #    grep "Vector  ${ELECTRONS}" "${i}" | sed -n 2p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
     IP=` grep "Total DFT energy" "${i}" | sed -n 2p | awk '{printf (" %f ",27.211*-($5-('$ENERGY')))}' `
@@ -31,6 +31,8 @@ do
 #    grep "Vector  ${ELECTRONS+1}" "${i}" | sed -n 4p | sed s/=/\ /g | awk '{printf (" %f ",$6*0.27211)}'
 
     EA=` grep "Total DFT energy" "${i}" | sed -n 3p | awk '{printf (" %f ",27.211*($5-('$ENERGY')))}' `
+
+    echo "HOMO: ${H} LUMO: ${L} IP: ${IP} EA: ${EA}"
 
     echo $H $L $IP $EA | awk '{print $1,$2,$3,$4,$3-$1,$4-$2}'
     #    grep "Root  1" "${i}" 
